75152fe6e9aefa5e748c205431ca4f3714f7dfac
SlimeGeekin <155208007+SlimeGeekin@users.noreply.github.com>
2024-10-25 12:52:02 -0400

    Initial commit

---
+++ src/main/java/me/eclipsorz/npc_quest3.java
@@ -0,0 +1,17 @@
+package me.eclipsorz;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class npc_quest3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
